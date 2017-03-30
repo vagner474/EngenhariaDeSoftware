68bdf7c4a372d3b60b6709865673bb44eda7e690
Vagner Santos da Silva <vagner.silva@dce.ufpb.br>
2017-03-29 21:48:03 -0300

    Commit para atualizar o ';'

--- src/EngS20162.java
+++ src/EngS20162.java
@@ -1,9 +1,10 @@
 
 public class EngS20162 {
+	
 	private String nome;
 	
 	public static void main (String [] args) {
 		
-		System.out.println("Soberano maior que a galinhada! kkkkkkk")
+		System.out.println("Soberano maior que a galinhada! kkkkkkk");
 	}
 }
